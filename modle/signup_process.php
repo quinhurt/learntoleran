56f464cffaa4d786a58032cd03dfa287a38d1f09
quinhurt <35904829+quinhurt@users.noreply.github.com>
2018-04-06 21:29:45 +1000

    student class added
    student class added

--- modle/signup_process.php
+++ modle/signup_process.php
@@ -7,7 +7,8 @@ $servername = "localhost";
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql = "INSERT INTO `user`
-         VALUES ('userID', '". $_POST['name']. "' , '". $_POST['lname']. "', '".$_POST['DOB']. "' , '". $_POST['username']. "' , '". $_POST['password']. "' , '2' , '1' );";
+         VALUES ('userID', '". $_POST['name']. "' , '". $_POST['lname']. "', '".$_POST['DOB']. "'
+           , '". $_POST['username']. "' , '". $_POST['password']. "' ,  '". $_POST['class']. "' , '2' , '1' );";
          $conn->exec($sql);
          echo "New record created successfully";
          }
--- view/signup.php
+++ view/signup.php
@@ -1,11 +1,30 @@
 
+<?php
 
+$sql_class = "SELECT * FROM class";
+$conn = dbConnect();
+$res_class = $conn->prepare($sql_class);
+$res_class->execute();
+
+
+ ?>
 
 <form action="modle\signup_process.php"  method="post">
   <input class="valid" type="text" required placeholder="first name" name="name" >
 <input class="valid" type="text"required placeholder="last name" name="lname" >
-<input type="text"  name"DOB" required placeholder="date of birth">
+<input type="text"  name="DOB" required placeholder="date of birth">
 <input class="valid" type="text"required placeholder="username" name="username" >
+<select name="class" style="width: 200px">
+            <?php
+            while ($row_class = $res_class->fetch(PDO::FETCH_ASSOC)) {
+                echo '<option value="' . $row_class['ClassID'] . '" ';
+
+                echo '>' . $row_class['className'] . '</option>';
+            }
+            ?>
+
+  </select>
+
 <input class="valid" type="text"required placeholder="password" name="password" id="pasword">
   <input type="submit" name="submit"  onclick="student()">
 </form>
