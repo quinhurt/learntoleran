9795dfed44ba9286048d07e7029a6ce313a050c5
quinhurt <35904829+quinhurt@users.noreply.github.com>
2018-05-14 16:45:38 +1000

    fix

--- view/js/script.js
+++ view/js/script.js
@@ -1,5 +1,5 @@
 
-function delItaem(elem){
+function edittaem(elem){
   if(confirm('are you sure')){
     ;
   }
--- view/update.php
+++ view/update.php
@@ -22,7 +22,7 @@ WHERE userID =".$_SESSION['userid'];
     echo '<input  class="form-control"  type="text" placeholder="last name"     pattern="[A-Za-z0.9]{3,20}"  name="surname" value="'.$surname.'" >';
     echo '<input  class="form-control"  type="text" placeholder="date of birth"  name="DOB" value="'.$DOB.'">';
     echo '<input  class="form-control"  type="text" placeholder="password"      pattern="[A-Za-z0.9]{3,20}" name="pasword" id="pasword" value= "'.$pasword.'">';
-    echo ' <input type="submit" name="updat"  onclick="delItaem(this)"  >';
+    echo ' <input type="submit" name="updat"  onclick="edittaem(this)"  >';
 ?>
 <br>
 <br>
